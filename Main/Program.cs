7fcacfd47fb942b891e7f3c76e932b4ba9c31109
Vladimir <vladimirtikhonov230@gmail.com>
2022-08-26 01:44:59 +0300

    hard work ended but not complete

--- Main/Program.cs
+++ Main/Program.cs
@@ -1,64 +1,137 @@
 ﻿Console.Clear();
 int counter = 0;
 double balance = 5000;
-bool verify = false;
 
-bool Invite(){
+
+bool CheckingInvite(){
+    bool verify = false;
+    int counter = 0;
     while (verify == false){
         Console.WriteLine ("Would you like to play?(y/n)");
         string? play = Console.ReadLine();
-        if (play is "" || play.ToLower() == "y") verify = true;
-        if (play.ToLower() == "n"){
+        if (play is "" || play?.ToLower() == "y") verify = true;
+        if (play?.ToLower() == "n"){
             Console.WriteLine ("See you soon! :D");
             Environment.Exit(0);
         }
-        if (play is not "" && play.ToLower() != "y" && play.ToLower() != "n") 
+        if (play is not "" && play?.ToLower() != "y" && play?.ToLower() != "n"){ 
+            Console.WriteLine ();
             Console.WriteLine ("Error, enter is acceptable answer");
-            //await Task.Delay(2000);
-       // Console.Clear();
+            counter++;
+        }
+        if (counter >= 3){
+           Console.Clear();
+           counter = 0; 
+        } 
     }
     return verify;
 }
 
-Console.WriteLine("The nickname must be no more than 10 characters long");
+int CheckingBid(double bank){
+    bool verify = false;
+    int valueBet;
+    while (verify == false){
+        Console.Write ("Enter bet: ");
+        string? bet = Console.ReadLine();
+        bool checkType = int.TryParse(bet, out valueBet);
+        Console.WriteLine(valueBet);
+        if (checkType && valueBet >= 100 && valueBet <= bank){ 
+            verify = true;
+            return valueBet;
+        }else{
+            Console.WriteLine();
+            Console.WriteLine("Enter as integer");
+            valueBet = -1;
+        }
+        if (valueBet > bank ){
+            Console.WriteLine();
+            Console.WriteLine("The bet cannot exceed the balance");
+        }
+        if (valueBet <= 0 ){
+            Console.WriteLine();
+            Console.WriteLine("The bet cannot be less than zero");
+        }
+        if (valueBet < 100 && valueBet > 0){
+            Console.WriteLine();
+            Console.WriteLine("The minimum bet is 100");
+        }
+        if (bet is ""){
+            Console.WriteLine();
+            Console.WriteLine("You didn't enter a bet");
+        }
+    }
+    return 0;
+}
+    
+
+Console.WriteLine("The nickname must be no more than 15 characters long");
+Console.Write ("Enter your nickname: ");
+string? nickname = Console.ReadLine();
+if (nickname is "") nickname = "Bass lover";
+else nickname = nickname?.Substring(0, 10);
+Console.Clear();
+
+bool confirmationInvite = CheckingInvite();
+while (confirmationInvite == true){
+    if(counter > 0){
+        CheckingInvite(); 
+    }
+    Console.Clear();
+    Console.WriteLine("========== Welcome to Casino Bass Ground ==========");
+    Console.Write($"Nickname: {nickname}            Balance: {balance}");
+
+    Console.WriteLine();
+    //Console.Write ("Enter bid: ");
+    //int bid = Convert.ToInt32 (Console.ReadLine());
+    int bid = CheckingBid(balance);
+
+    Console.Write ("Enter digit 1 - 10: ");
+    int digit = Convert.ToInt32 (Console.ReadLine());
+    Console.WriteLine ();
+    Console.WriteLine ("Wait...");
+    int number = new Random().Next(1, 11);
+    await Task.Delay(1000);
+    Console.WriteLine ($"The dropped digit: {number}");
+    Console.WriteLine ();
+    await Task.Delay(500);
+
+    if (digit == number){
+        double prize = Math.Round((bid * 0.3), 2,MidpointRounding.AwayFromZero);
+        balance += prize;
+        Console.WriteLine($"Congratulations! Your prize: {prize}"); 
+        Console.WriteLine("Wait 5 sec for restart");
+        await Task.Delay(5000);
+    }else{
+        Console.WriteLine ("Unfortunately your bid didn't go in");
+        Console.WriteLine("Wait 5 sec for restart");
+        balance -= bid;
+        await Task.Delay(5000);
+    }   
+counter++;
+}
+/*
 Console.Write ("Enter your nickname: ");
 string? nickname = Console.ReadLine();
 if (nickname is "") nickname = "Unknown";
-Console.Clear();
-
-Invite();
-
-if (verify == true){
-    while (verify == true){
-        if(counter > 0){
-            Invite(); 
-        }
-        Console.Clear();
-        Console.WriteLine("========== Welcome to Casino Bass Ground ==========");
-        Console.Write($"Nickname: {nickname}            Balance: {balance}");
-        Console.WriteLine();
-        Console.Write ("Enter bid: ");
-        int stavka = Convert.ToInt32 (Console.ReadLine());
-        Console.Write ("Enter digit 1 - 10: ");
-        int digit = Convert.ToInt32 (Console.ReadLine());
-        Console.WriteLine ();
-        Console.WriteLine ("Wait...");
-        int number = new Random().Next(1, 11);
-        await Task.Delay(1000);
-        Console.WriteLine ($"The dropped digit: {number}");
-        await Task.Delay(1000);
-        if (digit == number){
-            double prize = Math.Round((stavka * 0.3), 2,MidpointRounding.AwayFromZero);
-            balance += prize;
-            Console.WriteLine($"Congratulations! Your prize: {prize}"); 
-            Console.WriteLine("Wait 5 sec for restart");
-            await Task.Delay(5000);
-        }else{
-            Console.WriteLine ("Unfortunately your bid didn't go in");
-            Console.WriteLine("Wait 5 sec for restart");
-            balance -= stavka;
-            await Task.Delay(5000);
-        }
-    counter++;
+nickname = nickname.Substring(0, 10);
+ Console.Write($"Nickname: {nickname}");
+ 
+ while (true)
+{
+    Console.Write("Введите число: ");
+    string text = Console.ReadLine();
+    if (int.TryParse(text, out int number))
+    {
+        Console.WriteLine("Вы ввели число {0}", number);
+        break;
     }
+    Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
 }
+
+int i = 27;
+Console.WriteLine(i is System.IFormattable);  // output: True
+
+object iBoxed = i;
+Console.WriteLine(iBoxed is int);  // output: True
+Console.WriteLine(iBoxed is long);  // output: False
+*/
